339a27e8a95167e70163bfb197e3e04f15680037
Alex Wintle <alexwintle663@gmail.com>
2021-09-27 16:37:37 +0100

    added top_up method & tests

--- lib/Oystercard_class.rb
+++ lib/Oystercard_class.rb
@@ -6,4 +6,8 @@ class Oystercard
 
 attr_reader :balance
 
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,14 +2,33 @@ require 'oystercard_class'
 
 RSpec.describe Oystercard do
 
-  it 'checks that the Oystercard class exists' do
-    my_oystercard = Oystercard.new(10)
-    expect(my_oystercard.respond_to?(:oystercard))
+  before(:each) do
+    @my_oystercard_a = Oystercard.new(10)
+    @my_oystercard_b = Oystercard.new(20)
   end
 
-  it 'checks that a new card has a balance' do
-    my_oystercard = Oystercard.new(10)
-    expect(my_oystercard.balance).to eq 10
+  describe "#initialize" do
+    it 'checks that the Oystercard class exists' do
+      expect(@my_oystercard_a.respond_to?(:oystercard))
+    end
+  end
+
+  describe "#balance" do
+    it 'checks that a new card has a balance' do
+      expect(@my_oystercard_a.balance).to eq 10
+    end
+  end
+
+  describe "#top_up" do
+    it 'should add money to the balance, and take in the amount as an argument' do
+      expect(@my_oystercard_a.top_up(20)).to eq 30
+    end
+  end
+
+  describe "#top_up" do
+    it 'should add money to the balance, and take in the amount as an argument' do
+      expect(@my_oystercard_b.top_up(30)).to eq 50
+    end
   end
 
 end     
